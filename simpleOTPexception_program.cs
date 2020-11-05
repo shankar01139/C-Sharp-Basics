fbcdaa924851c41ab2344a20e337ce48c268c6e6
Shankaranarayanan <60654610+shankar01139@users.noreply.github.com>
2020-11-05 11:50:35 +0530

    new program added

---
+++ simpleOTPexception_program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+		class OTPException : Exception
+	{
+		public OTPException(string message) : base(message)
+		{
+		}
+	}
+	public class Program
+	{
+		public static void Main()
+		{
+			var otp = 3476;
+			Console.WriteLine("Enter Your Mobile Number To verify : ");
+			var mob_num = Convert.ToInt64(Console.ReadLine());
+			Console.WriteLine("Enter the Four Digit OTP you received on your mobile.... ");
+			var data = Convert.ToInt64(Console.ReadLine());
+			try
+			{
+				if(otp == data)
+				{
+					Console.WriteLine("Mobile Number Verified Successfully ...!");
+					Console.ReadLine();
+				}
+				else
+				{
+					throw (new OTPException("Enter valid OTP...!"));
+					Console.ReadLine();
+				}
+			}
+			catch (OTPException e)
+			{
+				Console.Write("Error occurred while Reading OTP....." + e.Message);
+				Console.ReadLine();
+			}
+		}
+	}
+}
